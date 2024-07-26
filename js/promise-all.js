d40a903a6bf100b8f67de1bf25da8216c39d7ad2
Vladimir Dunaev <vladimir.dunaev@efinancialcareers.com>
2024-07-26 15:08:15 +0300

    fix promise-all task

--- js/promise-all.js
+++ js/promise-all.js
@@ -1,28 +1,27 @@
 
-// async function promiseAll(promiseArray) {
-//     let responses = [];
-//
-//     for (let i=0; i<promiseArray.length; i++) {
-//         const res = await promiseArray[i];
-//         responses.push(res);
-//     }
-//
-//     return responses;
-// }
-//
-// const promise1 = new Promise((resolve, reject) => {
-//     setTimeout(resolve, 100, 'first promise resolved');
-// });
-//
-// const promise2 = new Promise((resolve, reject) => {
-//     setTimeout(resolve, 200, 'second promise rejected');
-// });
-//
-// const promise3 = new Promise((resolve, reject) => {
-//     setTimeout(resolve, 300, 'third promise resolved');
-// });
-//
-// promiseAll(promise1, promise2, promise3).then(array => {
-//     console.log('then', array)
-//     array.forEach((res) => console.log(res))
-// });
+async function promiseAll(promiseArray) {
+    const response = [];
+
+    for (let i = 0; i < promiseArray.length; i++) {
+        const res = await promiseArray[i];
+        response.push(res);
+    }
+
+    return response;
+}
+
+const promise1 = new Promise((resolve) => {
+    setTimeout(resolve, 100, 'first promise resolved');
+});
+
+const promise2 = new Promise((resolve) => {
+    setTimeout(resolve, 200, 'second promise rejected');
+});
+
+const promise3 = new Promise((resolve) => {
+    setTimeout(resolve, 300, 'third promise resolved');
+});
+
+promiseAll([promise1, promise2, promise3]).then(array => {
+    array.forEach((res) => console.log(res))
+});
