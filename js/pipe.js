4a7a310f7df62f76d7fa872b169b1c9df159673f
Vladimir Dunaev <vladimir.dunaev@efinancialcareers.com>
2024-08-18 23:05:42 +0300

    add new tasks

--- js/pipe.js
+++ js/pipe.js
@@ -1,7 +1,8 @@
 const times = (y) => (x) => x * y;
 const plus = (y) => (x) => x + y;
 
-// const pipe = (arr) => (initialValue) => arr.reduce((acc, fn) => fn(acc), initialValue);
+// const pipe = (arr) => (initialValue) =>
+//     arr.reduce((acc, fn) => fn(acc), initialValue);
 
 const pipe = function(arr) {
     return function(initialValue) {
@@ -11,5 +12,8 @@ const pipe = function(arr) {
     };
 };
 
-console.log(pipe([times(2), times(3)])(2)); // 12
-console.log(pipe([times(2), plus(3), times(3)])(2)); // 28
+console.log(pipe([times(2), times(3)])
+    (2)); // 12
+
+console.log(pipe([times(2), plus(3), times(3)])
+    (2)); // 28
